10b470b99c3a89b46e326347349d6e0c707f8076
Jade Asmus <jadeasmus99@gmail.com>
2021-11-05 10:46:56 -0400

    Show timer options

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  // const [time, setTime] = useState(time);
+  const [showTimes, setShowTimes] = useState(false);
+
+  // const pickTime = () => {
+  //   const pickFrom = [2, 5, 10, 15]
+  //   setTime(time)
+  // }
+
+
   return (
     <div className="App">
-      <p> Hello world </p>
+      <h1></h1>
+      <button onClick={ () => setShowTimes(true) }> Set timer </button>
+      { showTimes ? 
+        <div className="times">
+          <button>2</button>
+          <button>5</button>
+          <button>10</button>
+          <button>15</button>
+        </div>
+        : null
+      }
+      <button>Stop</button>
     </div>
   );
 }
